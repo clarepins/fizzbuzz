4b52fb77b2786dd24bfe67a362e72158d46f672b
Dora Kemeny <dory1318@gmail.com>
2019-01-29 12:21:47 +0000

    adding 7 to test

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,7 @@
 def fizzbuzz(number)
-    'fizz'
-end 
+  if number == 3
+    return "fizz"
+  elsif number == 5
+    return "buzz"
+  end
+end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,11 +1,16 @@
 require 'fizzbuzz'
 
-describe "fizzbuzz" do 
+describe "fizzbuzz" do
     it 'returns "fizz" when passed 3' do
         expect(fizzbuzz(3)).to eq "fizz"
     end
-    
-    it 'returns "buzz" when passed 5' do 
+
+    it 'returns "buzz" when passed 5' do
         expect(fizzbuzz(5)).to eq "buzz"
-    end 
+    end
+
+    it 'returns 7 when passed 7' do
+      expect(fizzbuzz(7)).to eq 7
+    end
+    
 end
