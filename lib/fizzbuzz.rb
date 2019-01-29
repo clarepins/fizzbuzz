56f7e749da51f14cd8e5fd8ac449001228e8c647
Clare Pinder <ec2-user@ip-172-31-15-208.eu-west-1.compute.internal>
2019-01-29 12:31:10 +0000

    added return fizzbuzz for multiples of 3 and 5

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,5 +1,7 @@
 def fizzbuzz(number)
-  if number == 3
+  if number % 3 == 0 && number % 5 == 0
+      return "fizzbuzz"
+  elsif number == 3
     return "fizz"
   elsif number == 5
     return "buzz"
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -5,6 +5,10 @@ describe "fizzbuzz" do
         expect(fizzbuzz(3)).to eq "fizz"
     end
 
+    it 'returns "fizz" when passed 6' do
+        expect(fizzbuzz(6)).to eq "fizz"
+    end 
+    
     it 'returns "buzz" when passed 5' do
         expect(fizzbuzz(5)).to eq "buzz"
     end
@@ -16,5 +20,7 @@ describe "fizzbuzz" do
     it 'returns fizzbuzz when passed 15' do
         expect(fizzbuzz(15)).to eq "fizzbuzz"
     end
+    
+    
     
 end
